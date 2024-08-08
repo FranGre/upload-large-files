0f57fc63518149735528f57421d62b16a251a65b
fran gregori <frangregori61@gmail.com>
2024-08-08 23:35:11 +0200

    implemented resumable on frontend

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <h1>hola</h1>
+<div x-data="uploader">
+    <input type="file" x-ref="file" />
+    <button x-on:click="upload">Upload</button>
 </div>
+
+
+<script src=" https://cdn.jsdelivr.net/npm/resumablejs@1.1.0/resumable.min.js "></script>
+
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('uploader', () => ({
+            upload(){
+                const file = this.$refs.file.files[0]
+
+                var resumable = new Resumable({
+                    headers: {
+                        'X-CSRF-TOKEN' : "{{ csrf_token() }}"
+                    },
+                    target: "{{route('upload.large.video')}}",
+                    chunkSize: 5 * 1024 * 1024,
+                    testChunks: false
+                })
+                
+                resumable.addFile(file)
+
+                resumable.on('fileAdded', function(file, event){
+                    resumable.upload()
+                })
+            }
+        }))
+    })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UploadLargeVideoController;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class)->name('welcome');
+Route::post('/upload/large/video', UploadLargeVideoController::class)->name('upload.large.video');
