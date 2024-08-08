5b053ead30d0ec69bcd3ac989f93160cb69b8db8
fran gregori <frangregori61@gmail.com>
2024-08-09 00:10:40 +0200

    Implementado laravel-chunk-upload en el backend, ya funciona :)

--- app/Http/Controllers/UploadLargeVideoController.php
+++ app/Http/Controllers/UploadLargeVideoController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Pion\Laravel\ChunkUpload\Handler\ResumableJSUploadHandler;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class UploadLargeVideoController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $reciver = new FileReceiver($request->file, $request, ResumableJSUploadHandler::class);
+
+        $save = $reciver->receive();
+
+        if ($save->isFinished()) {
+            $file = $save->getFile();
+
+            $orginalName = $file->getClientOriginalName();
+
+            $file->move(storage_path('app/chunks'), $orginalName);
+        }
+        $handler = $save->handler();
+
+        return response()->json(['progress' => $handler->getPercentageDone()]);
     }
 }
